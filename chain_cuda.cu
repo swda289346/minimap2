80a68a6d64df3529ec5a1d6b6072772e1f0869f5
Yunju Lee <yunju@psu.edu>
2019-04-08 11:36:12 -0400

    Optimized CUDA implementation for mm_chain_dp_fill

--- chain_cuda.cu
+++ chain_cuda.cu
@@ -9,14 +9,16 @@ extern "C" {
 void mm_chain_dp_fill_cuda(int max_dist_x, int max_dist_y, int bw, int max_skip, int max_iter, int n_segs, int is_cdna, int64_t n, mm128_t *a, float avg_qspan, int32_t *f, int32_t *t, int32_t *p, int32_t *v)
 {
 	const int blockDim = 32;
-	const int maxBlockDim = 1024;
 	int numBlocks = (max_iter+blockDim-1)/blockDim;
-	int64_t i;
+	int64_t j;
 	int32_t *f_cuda, *t_cuda, *p_cuda, *v_cuda;
 	mm128_t *a_cuda;
-	int64_t *mem_max_j;
-	int32_t *mem_max_f;
 	cudaError_t result;
+	for (uint64_t i=0;i<n;i++)
+	{
+		v[i] = f[i] = a[i].y>>32&0xff; // NB: only 8 bits of span is used!!!
+		p[i] = -1;
+	}
 	result = cudaMalloc(&f_cuda, sizeof(int32_t)*n);
 	assert(result==cudaSuccess);
 	result = cudaMalloc(&t_cuda, sizeof(int32_t)*n);
@@ -37,18 +39,10 @@ void mm_chain_dp_fill_cuda(int max_dist_x, int max_dist_y, int bw, int max_skip,
 	assert(result==cudaSuccess);
 	result = cudaMemcpy(a_cuda, a, sizeof(mm128_t)*n, cudaMemcpyHostToDevice);
 	assert(result==cudaSuccess);
-	result = cudaMalloc(&mem_max_j, sizeof(int64_t)*max_iter);
-	assert(result==cudaSuccess);
-	result = cudaMalloc(&mem_max_f, sizeof(int32_t)*max_iter);
-	assert(result==cudaSuccess);
 	result = cudaDeviceSynchronize();
 	assert(result==cudaSuccess);
-	for (i = 0; i < n; ++i) {
-		uint64_t ri = a[i].x;
-		int32_t qi = (int32_t)a[i].y, q_span = a[i].y>>32&0xff; // NB: only 8 bits of span is used!!!
-		int32_t sidi = (a[i].y & MM_SEED_SEG_MASK) >> MM_SEED_SEG_SHIFT;
-		mm_chain_dp_fill_cuda_score<<<numBlocks, blockDim>>>(i, ri, qi, q_span, sidi, mem_max_j, mem_max_f, max_dist_x, max_dist_y, bw, max_iter, n_segs, is_cdna, a_cuda, avg_qspan, f_cuda, t_cuda, p_cuda);
-		mm_chain_dp_fill_cuda_max<<<1, maxBlockDim>>>(i, mem_max_j, mem_max_f, max_iter, f_cuda, p_cuda, v_cuda);
+	for (j = 0; j < n-1; ++j) {
+		mm_chain_dp_fill_cuda_optimized<<<numBlocks, blockDim>>>(j, n, max_dist_x, max_dist_y, bw, max_iter, n_segs, is_cdna, a_cuda, avg_qspan, f_cuda, t_cuda, p_cuda, v_cuda);
 	}
 	result = cudaDeviceSynchronize();
 	assert(result==cudaSuccess);
@@ -65,8 +59,6 @@ void mm_chain_dp_fill_cuda(int max_dist_x, int max_dist_y, int bw, int max_skip,
 	cudaFree(p_cuda);
 	cudaFree(v_cuda);
 	cudaFree(a_cuda);
-	cudaFree(mem_max_j);
-	cudaFree(mem_max_f);
 }
 
 }
--- chain_cuda_kernel.h
+++ chain_cuda_kernel.h
@@ -3,5 +3,6 @@
 
 __global__ void mm_chain_dp_fill_cuda_score(int64_t i, uint64_t ri, int32_t qi, int32_t q_span, int32_t sidi, int64_t *mem_max_j, int32_t *mem_max_f, int max_dist_x, int max_dist_y, int bw, int max_iter, int n_segs, int is_cdna, mm128_t *a, float avg_qspan, int32_t *f, int32_t *t, int32_t *p);
 __global__ void mm_chain_dp_fill_cuda_max(int i, int64_t *mem_max_j, int32_t *mem_max_f, int max_iter, int32_t *f, int32_t *p, int32_t *v);
+__global__ void mm_chain_dp_fill_cuda_optimized(int64_t j, int64_t n, int max_dist_x, int max_dist_y, int bw, int max_iter, int n_segs, int is_cdna, mm128_t *a, float avg_qspan, int32_t *f, int32_t *t, int32_t *p, int32_t *v);
 
 #endif
